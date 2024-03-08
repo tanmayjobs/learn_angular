68879a0bbe947970507dfb341f5c6901660d5ebb
tanmayjobs <tanmayjobs@gmail.com>
2024-03-08 14:32:25 +0530

    Empty

--- tryfail/src/app/navbar/navbar.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    "selector": "navbar-component",
-    "templateUrl": "navbar.component.html",
-    "styleUrl": "navbar.component.css"
-})
-export class NavbarComponent {}
