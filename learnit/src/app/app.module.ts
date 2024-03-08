ef56388c124adb30654fc74c3e7db73bea17ddd7
tanmayjobs <tanmayjobs@gmail.com>
2024-03-08 15:49:47 +0530

    navbar

---
+++ learnit/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarItemComponent } from './navbar-item/navbar-item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    NavbarItemComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ learnit/src/app/navbar-item/navbar-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'navbar-item',
+  templateUrl: './navbar-item.component.html',
+  styleUrl: './navbar-item.component.css'
+})
+export class NavbarItemComponent {
+  
+}
